c257153b8dec7e44218f6f6f6fdab2dae5ed1b1f
Liam Lawrence <and3212@gmail.com>
2018-06-23 23:59:04 -0400

    Update ascii_to_dec.cpp

--- src/ascii_to_dec.cpp
+++ src/ascii_to_dec.cpp
@@ -13,20 +13,20 @@
 
 int main() {
     std::string s;
-	int value = 0;
+    int value = 0;
 
     // Loops through the five words
-	for (int i = 0; i < 5; i++) {
-		std::cout << "Word " << i + 1 << ": ";
-		std::cin >> s;
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Word " << i + 1 << ": ";
+        std::cin >> s;
 
-        // Sums their ASCII values
-		for (int j = 0; j < s.length(); j++) {
-			value += s.at(j);		
-		}
+    // Sums their ASCII values
+    for (int j = 0; j < s.length(); j++) {
+        value += s.at(j);		
+    }
 
-        // Prints the value out and resets it for the next word
-		std::cout << "      - " << value << "\n\n";
-		value = 0;
-	}
+    // Prints the value out and resets it for the next word
+        std::cout << "      - " << value << "\n\n";
+        value = 0;
+    }
 }
